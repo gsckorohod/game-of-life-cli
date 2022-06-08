77113ecdaaf1e947e2113bc953e1ead48cf4252e
gsckorohod <gsckorohod@yandex.ru>
2022-06-08 19:50:08 +0300

    Clean up

--- target/debug/build/libc-676f6e5c87aa1f0c/build_script_build-676f6e5c87aa1f0c.d
+++
@@ -1,5 +0,0 @@
-/home/mayday/Projects/game-of-life/target/debug/build/libc-676f6e5c87aa1f0c/build_script_build-676f6e5c87aa1f0c: /home/mayday/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.126/build.rs
-
-/home/mayday/Projects/game-of-life/target/debug/build/libc-676f6e5c87aa1f0c/build_script_build-676f6e5c87aa1f0c.d: /home/mayday/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.126/build.rs
-
-/home/mayday/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.126/build.rs:
--- target/debug/game-of-life.d
+++
@@ -1,1 +0,0 @@
-/home/mayday/Projects/game-of-life/target/debug/game-of-life: /home/mayday/Projects/game-of-life/src/main.rs
